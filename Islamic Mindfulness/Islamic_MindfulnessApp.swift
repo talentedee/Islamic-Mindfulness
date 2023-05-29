da520ae57c7c0d8255544899e5306c41244b67f0
Mohamad Hassan <moyasserh95@gmail.com>
2023-05-29 17:20:46 -0400

    Initial Commit

---
+++ Islamic Mindfulness/Islamic_MindfulnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Islamic_MindfulnessApp.swift
+//  Islamic Mindfulness
+//
+//  Created by Mohamad Hassan on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Islamic_MindfulnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
